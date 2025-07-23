f151690605f7de997b4bf62d6c9cd2ec83d087fc
Kapil Baraskar <kapilbaraskar91@gmail.com>
2025-07-24 00:01:59 +0530

    adds methods in workout store

--- Go_Pro/fm_project/internal/store/workout_store.go
+++ Go_Pro/fm_project/internal/store/workout_store.go
@@ -34,3 +34,45 @@ type WorkoutStore interface {
 	CreateWorkout(*Workout) (*Workout, error)
 	GetWorkoutByID(id int64) (*Workout, error)
 }
+
+func (pg *PosgresWorkoutStore) CreateWorkout(workout *Workout) (*Workout, error) {
+	tx, err := pg.db.Begin()
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	query :=
+		`
+	INSERT INTO workouts (user_id, title, description, duration_minutes, calories_burned)
+  	VALUES ($1, $2, $3, $4, $5)
+  	RETURNING id
+	`
+
+	err = tx.QueryRow(query, workout.Title, workout.Description, workout.DurationMinutes, workout.CaloriesBurned).Scan(&workout.ID)
+	if err != nil {
+		return nil, err
+	}
+	for _, entry := range workout.Entries {
+		query := `
+	INSERT INTO workout_entries (workout_id, exercise_name, sets, reps, duration_seconds, weight, notes, order_index)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+    RETURNING id
+		`
+		err = tx.QueryRow(query, workout.ID, entry.ExerciseName, entry.Sets, entry.Reps, entry.DurationSeconds, entry.Weight, entry.Notes, entry.OrderIndex).Scan(&entry.ID)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return workout, nil
+}
+
+func (pg *PosgresWorkoutStore) GetWorkoutByID(id int) (*Workout, error) {
+	workout := &Workout{}
+	return workout, nil
+}
